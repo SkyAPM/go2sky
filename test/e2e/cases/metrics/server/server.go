a06fc71dca9f0e9603f00e734ac423433c33ef01
easonyipj <yipingjian@qq.com>
2022-10-08 21:03:02 +0800

    [feat]add e2e test

---
+++ test/e2e/cases/metrics/server/server.go
@@ -0,0 +1,58 @@
+//
+// Copyright 2022 SkyAPM org
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"github.com/SkyAPM/go2sky"
+	"github.com/SkyAPM/go2sky/reporter"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	oap     = "mockoap:19876"
+	service = "http-server"
+)
+
+func main() {
+	report, err := reporter.NewGRPCReporter(oap, reporter.WithMeterCollectPeriod(1*time.Second))
+	if err != nil {
+		log.Fatalf("crate grpc reporter error: %v \n", err)
+	}
+
+	_, err = go2sky.NewTracer(service, go2sky.WithReporter(report))
+	if err != nil {
+		log.Fatalf("crate tracer error: %v \n", err)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	route := http.NewServeMux()
+	route.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("Hello World!"))
+	})
+
+	//sm, err := httpPlugin.NewServerMiddleware(tracer)
+	//if err != nil {
+	//	log.Fatalf("create server middleware error %v \n", err)
+	//}
+	err = http.ListenAndServe(":8080", route)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
