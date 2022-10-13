bcb5bb4e26360fc6e5f66106ae22879153fed4c2
easonyipj <yipingjian@qq.com>
2022-10-13 16:11:34 +0800

    [feat]update workflows/e2e.yaml to add Golang Runtime Metrics e2e test file

---
+++ test/e2e/cases/metrics/server/server.go
@@ -0,0 +1,59 @@
+//
+// Copyright 2022 SkyAPM org
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/SkyAPM/go2sky"
+	httpPlugin "github.com/SkyAPM/go2sky/plugins/http"
+	"github.com/SkyAPM/go2sky/reporter"
+)
+
+const (
+	oap     = "127.0.0.1:11800"
+	service = "http-server"
+)
+
+func main() {
+	meterInterval := 5 * time.Second
+	report, err := reporter.NewGRPCReporter(oap, reporter.WithMeterCollectPeriod(meterInterval))
+	if err != nil {
+		log.Fatalf("crate grpc reporter error: %v \n", err)
+	}
+
+	tracer, err := go2sky.NewTracer(service, go2sky.WithReporter(report))
+	if err != nil {
+		log.Fatalf("crate tracer error: %v \n", err)
+	}
+
+	route := http.NewServeMux()
+	route.HandleFunc("/helloserver", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("Hello World!"))
+	})
+
+	sm, err := httpPlugin.NewServerMiddleware(tracer)
+	if err != nil {
+		log.Fatalf("create server middleware error %v \n", err)
+	}
+	err = http.ListenAndServe(":8080", sm(route))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ test/e2e/script/prepare/setup-e2e-shell/install-yq.sh
@@ -0,0 +1,26 @@
+# Copyright 2022 SkyAPM org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+if ! command -v yq &> /dev/null; then
+  # prepare base dir
+  BASE_DIR=/tmp/skywalking-infra-e2e/swctl
+  BIN_DIR=/usr/local/bin
+  mkdir -p $BASE_DIR && cd $BASE_DIR
+
+  curl -kLo yq.tar.gz https://github.com/mikefarah/yq/archive/v4.14.1.tar.gz
+  tar -zxf yq.tar.gz --strip=1
+  go install && go build -ldflags -s && cp yq $BIN_DIR/
+fi
